0bc34639226c90807fdff48108737fc5f5fa07f4
Amitava Choudhury <60097810+amitavachoudhury@users.noreply.github.com>
2021-08-20 11:23:10 +0530

    Add files via upload

---
+++ Introduction.php
@@ -0,0 +1,14 @@
+Client Server Architecture
+World wide web HTTP
+A Document Type Declaration, or DOCTYPE for short, is an instruction to the web browser 
+about the version of markup language in which a web page is written.
+<!DOCTYPE html>
+Hypertext means machine readable text and Markup means to structure it in a specific format.
+
+ASCII editor: notepad; notepad++, brackets, any other IDE available
+
+save filename.html (html) run on your browser(.htm)
+for php files: filename.php
+
+can we save HTML file with .php extn??
+Yes, 
